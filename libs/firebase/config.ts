712593613274199ab4c4aeffe625c8be9a18f2bf
Forhad Hossain <forhadhossain60@outlook.com>
2024-05-17 15:07:01 +0600

    updated firebase config

--- libs/firebase/config.ts
+++ libs/firebase/config.ts
@@ -2,12 +2,12 @@ import { getAuth } from 'firebase/auth';
 import { initializeApp, getApps } from 'firebase/app';
 
 const firebaseConfig = {
-  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
-  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  apiKey: "AIzaSyBdFeuOwBpB7sVdtKjb83Pe70VYW5PNHZ0",
+  authDomain: "redsouls-org.firebaseapp.com",
+  projectId: "redsouls-org",
+  storageBucket: "redsouls-org.appspot.com",
+  messagingSenderId: "784523304036",
+  appId: "1:784523304036:web:f7ca254e236af1aee21371",
 };
 
 const firebaseApp =
